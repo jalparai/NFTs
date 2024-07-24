04d5fbde995c3a3ec1e6ea3e40ee3ab112254996
Rohit Rai <161055240+Rohitrai12@users.noreply.github.com>
2024-07-24 14:31:38 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Header from "./components/headere";
+import Banner from "./components/banner";
+
+export default function Home() {
+  return (
+<div>
+    <Header />
+    <Banner />
+</div>
+  );
+}
